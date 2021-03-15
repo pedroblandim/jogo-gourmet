74694be892974976aca48ebee5d8e21dcea11303
Pedro Blandim <pedro.blandim@seatecnologia.com.br>
2021-03-14 22:24:06 -0300

    Implementando JOptionPaneUI

--- src/com/objective/jogo/gourmet/JogoGourmetApplication.java
+++ src/com/objective/jogo/gourmet/JogoGourmetApplication.java
@@ -2,23 +2,19 @@ package com.objective.jogo.gourmet;
 
 import com.objective.jogo.gourmet.api.ControladorDoJogo;
 import com.objective.jogo.gourmet.api.InterfaceDoUsuario;
-import com.objective.jogo.gourmet.api.JogoGourmet;
 import com.objective.jogo.gourmet.app.ControladorDoJogoImpl;
-import com.objective.jogo.gourmet.app.JogoGourmetImpl;
-import com.objective.jogo.gourmet.app.LinhaDeComandoUI;
+import com.objective.jogo.gourmet.app.JOptionPaneUI;
+import com.objective.jogo.gourmet.app.JogoGourmet;
 
-// TODO JOptionPaneIU
-// TODO Resolver "Resource leak: 'scanner' is never closed" em LinhaDeComandoUI
-// TODO Escrever javadocs (avaliar necessidade)
 
 public class JogoGourmetApplication {
 
 	public static void main(String[] args) {
 		ControladorDoJogo controladorDoJogo = new ControladorDoJogoImpl();
-//		InterfaceDoUsuario _interface = new JOptionPanelUI();
-		InterfaceDoUsuario _interface = new LinhaDeComandoUI(); // Facilitar debug
+		InterfaceDoUsuario _interface = new JOptionPaneUI();
+//		InterfaceDoUsuario _interface = new LinhaDeComandoUI(); // Facilitar debug
 		
-		JogoGourmet jogo = new JogoGourmetImpl(controladorDoJogo, _interface);
+		JogoGourmet jogo = new JogoGourmet(controladorDoJogo, _interface);
 		
 		jogo.iniciar();
 	}
--- src/com/objective/jogo/gourmet/app/JOptionPaneUI.java
+++ src/com/objective/jogo/gourmet/app/JOptionPaneUI.java
@@ -1,45 +1,67 @@
 package com.objective.jogo.gourmet.app;
 
+import java.awt.Component;
+
+import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
 import com.objective.jogo.gourmet.api.InterfaceDoUsuario;
 
 public class JOptionPaneUI implements InterfaceDoUsuario {
 
+	private static final String TITLE = "Jogo Gourmet";
+	
+	private static final String FRASE_INICIAL 						= "Pense em um prato que gosta";
+	private static final String FRASE_ACERTO 						= "Acertei de novo!";
+	private static final String FRASE_VERIFICAR_ADJETIVO 			= "O prato que você pensou é %s?";
+	private static final String FRASE_VERIFICAR_PRATO_ENCONTRADO 	= "O prato que você pensou é %s?";
+	private static final String FRASE_REQUISITAR_NOVO_ADJETIVO 		= "%s é _______ mas %s não.";
+	private static final String FRASE_REQUISITAR_NOVO_PRATO 		= "Qual prato você pensou?";
+	
+	private final JFrame frame = new JFrame();
+	private static final String[] simOuNaoOptions = {"Sim", "Não"};
+	
+	
 	@Override
-	public void apresentarFraseInicial() {
-		// TODO Auto-generated method stub
-		
+	public void apresentarFraseInicial() {  
+	    JOptionPane.showMessageDialog(frame, FRASE_INICIAL, TITLE, JOptionPane.PLAIN_MESSAGE);  
 	}
 
 	@Override
 	public void apresentarFraseDeAcerto() {
-		// TODO Auto-generated method stub
-		
+	    JOptionPane.showMessageDialog(frame, FRASE_ACERTO, TITLE, JOptionPane.DEFAULT_OPTION);  
 	}
 
 	@Override
 	public boolean verificarAdjetivo(String adjetivo) {
-		// TODO Auto-generated method stub
-		return false;
+		String pergunta = String.format(FRASE_VERIFICAR_ADJETIVO, adjetivo);
+	    return perguntarSimOuNao(pergunta);  
 	}
 
 	@Override
-	public boolean verificarPratoEncontrado(String pergunta) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean verificarPratoEncontrado(String pratoEncontrado) {
+		String pergunta = String.format(FRASE_VERIFICAR_PRATO_ENCONTRADO, pratoEncontrado);
+	    return perguntarSimOuNao(pergunta);  
 	}
 
 	@Override
 	public String requisitarNovoAdjetivo(String novoPrato, String pratoEncontrado) {
-		// TODO Auto-generated method stub
-		return null;
+		String pergunta = String.format(FRASE_REQUISITAR_NOVO_ADJETIVO, novoPrato, pratoEncontrado);
+		return requisitarValor(pergunta);
 	}
 
 	@Override
 	public String requisitarNovoPrato() {
-		// TODO Auto-generated method stub
-		return null;
+		String pergunta = String.format(FRASE_REQUISITAR_NOVO_PRATO);
+		return requisitarValor(pergunta);
 	}
-
+	
+	private boolean perguntarSimOuNao(String pergunta) {
+		Component fr = new JFrame();
+		return JOptionPane.showOptionDialog(fr, pergunta, TITLE, JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, simOuNaoOptions, simOuNaoOptions[0]) == JOptionPane.YES_OPTION;
+	}
+	
+	private String requisitarValor(String pergunta) {
+		return (String)JOptionPane.showInputDialog(frame, pergunta, TITLE, JOptionPane.QUESTION_MESSAGE); 
+	}
 }
